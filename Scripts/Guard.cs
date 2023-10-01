c7402b88c321928cc47e70158917043d067cffe0
Wenzav <aubincarl@hotmail.com>
2023-09-30 20:50:40 -0400

    Guard detection

--- Scripts/Guard.cs
+++ Scripts/Guard.cs
@@ -1,21 +1,25 @@
 using Godot;
 using System.Collections.Generic;
-
+using System;
 public class Guard : KinematicBody2D
 {
     [Export]
     private float moveSpeed = 10f;
     [Export]
     private float idleTimePerPatrolPoint = 3f;
+    [Export]
+    private float detectionTimerMax = 1.0f;
 
     private AnimatedSprite animatedSprite;
     private string idleAnimation = "idle_up";
     private Path2D path2D;
     private PathFollow2D pathFollow2D;
+    private Area2D detectionArea;
     private List<Vector2> patrolPoints = new List<Vector2>();
     private int currentPatrolPoint = 1;
     private Vector2 lastPosition;
-    private Vector2 currentDirection = Vector2.Zero;
+    private bool isPlayerDetected = false;
+    private float detectionTimer;
     public float currentIdleTimer = 0f;
 
     public override void _Ready()
@@ -23,13 +27,18 @@ public class Guard : KinematicBody2D
         animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
         pathFollow2D = GetParent() as PathFollow2D;
         path2D = pathFollow2D.GetParent() as Path2D;
+        detectionArea = GetNode<Area2D>("DetectionArea");
+        detectionTimer = detectionTimerMax;
         lastPosition = GlobalPosition;
         PreparePatrolPoint();
     }
 
     public override void _Process(float delta)
     {
-        Patrol(delta);
+        if (!isPlayerDetected)
+            Patrol(delta);
+        else
+            PlayerDetected(delta);
     }
 
     private void PreparePatrolPoint()
@@ -46,26 +55,48 @@ public class Guard : KinematicBody2D
     {
         Vector2 difference = pathFollow2D.Position - patrolPoints[currentPatrolPoint];
 
-        if (currentIdleTimer > 0f)
+        if(detectionTimer < detectionTimerMax)
         {
-            currentIdleTimer -= delta;
-            UpdateIdleAnimation(lastPosition - GlobalPosition);
-        }
-        else if (difference.Length() < 2f)
-        {
-            currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Count;
-            currentIdleTimer = idleTimePerPatrolPoint;
+            UpdateIdleAnimation();
+            detectionTimer += delta;
+            
+            if (detectionTimer > detectionTimerMax)
+                detectionTimer = detectionTimerMax;
         }
         else
         {
-            pathFollow2D.Offset = pathFollow2D.Offset + moveSpeed * delta;
-            UpdateWalkingAnimation(lastPosition - GlobalPosition);
+            if (currentIdleTimer > 0f)
+            {
+                currentIdleTimer -= delta;
+                UpdateIdleAnimation();
+            }
+            else if (difference.Length() < 2f)
+            {
+                currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Count;
+                currentIdleTimer = idleTimePerPatrolPoint;
+            }
+            else
+            {
+                pathFollow2D.Offset = pathFollow2D.Offset + moveSpeed * delta;
+                UpdateWalkingAnimation(lastPosition - GlobalPosition);
+            }
         }
 
         lastPosition = GlobalPosition;
     }
 
-    private void UpdateIdleAnimation(Vector2 direction)
+    private void PlayerDetected(float delta)
+    {
+        UpdateIdleAnimation();
+        detectionTimer -= delta;
+
+        if (detectionTimer <= 0)
+        {
+            detectionTimer = 0.0f;
+        }
+    }
+
+    private void UpdateIdleAnimation()
     {
         animatedSprite.Animation = idleAnimation;
 
@@ -78,23 +109,27 @@ public class Guard : KinematicBody2D
         if (direction.x < 0 && Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
         {
             animatedSprite.Animation = "walk_left";
+            detectionArea.RotationDegrees = 270.0f;
             idleAnimation = "idle_left";
             animatedSprite.FlipH = false;
         }
         if (direction.x > 0 && Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
         {
             animatedSprite.Animation = "walk_right";
+            detectionArea.RotationDegrees = 90.0f;
             idleAnimation = "idle_right";
             animatedSprite.FlipH = true;
         }
         if (direction.y > 0 && Mathf.Abs(direction.y) > Mathf.Abs(direction.x))
         {
             animatedSprite.Animation = "walk_up";
+            detectionArea.RotationDegrees = 180.0f;
             idleAnimation = "idle_up";
         }
         if (direction.y < 0 && Mathf.Abs(direction.y) > Mathf.Abs(direction.x))
         {
             animatedSprite.Animation = "walk_down";
+            detectionArea.RotationDegrees = 0.0f;
             idleAnimation = "idle_down";
         }
 
@@ -102,22 +137,16 @@ public class Guard : KinematicBody2D
             animatedSprite.Play();
     }
 
-    private void OnPlayerDetected(Area2D player)
-    {
-        GD.Print("Player detected!");
-    }
-
     private void OnDetectionAreaEntered(Node2D area)
     {
         if (area.IsInGroup("player"))
         {
-            GD.Print("player detected");
-            // logique ici pour le '?', alerter autres gardes,
+            isPlayerDetected = true;
         }
     }
 
     private void OnDetectionAreaExited(Area2D player)
     {
-
+        isPlayerDetected = false;
     }
 }
